6335a098b7b3099aeb9e2750302ea8365aef108c
Luna948 <hrjsantoshi2006@gmail.com>
2025-02-23 11:29:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,21 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById("container").appendChild(renderer.domElement);
 
 // Lighting
-const light = new THREE.AmbientLight(0xffffff, 1.5);
-scene.add(light);
+const ambientLight = new THREE.AmbientLight(0xffffff, 2);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+directionalLight.position.set(1, 1, 1);
+scene.add(directionalLight) 
 
 // Load Lumi-Chan GLB Model
 const loader = new THREE.GLTFLoader();
-loader.load('https://raw.githubusercontent.com/Luna984/LumiChan-3D/main/LumiChan.glb', function (gltf) {
+loader.load('https://raw.githubusercontent.com/Luna984/LumiChan-3D/main/Lumi.glb', function (gltf) {
     let model = gltf.scene;
     model.position.set(0, -1, 0);  // Adjust model position
     scene.add(model);
+
+    console.log("Lumi-Chan Loaded Successfully!") 
 
     // Rotate Animation
     function animateModel() {
